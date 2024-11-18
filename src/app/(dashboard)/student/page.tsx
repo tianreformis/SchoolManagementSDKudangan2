34c27a514520c019903d2d1e7b8937d0d254d372
tianreformis <tianreformis@gmail.com>
2024-11-18 11:21:00 +0800

    layout : adding students layouts

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,9 +1,25 @@
+import Announcements from "@/components/Announcements"
+import EventCalendar from "@/components/EventCalendar"
 import React from "react"
 
 const StudentPage
  = () => {
   return (
-    <div className=''>StudentPage
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* left */}
+      <div className="w-full x:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="font-semibold text-xl">Schedule (4A)</h1>
+
+
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   )
 }
