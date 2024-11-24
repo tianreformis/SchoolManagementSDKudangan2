e3b34064df4f92e9bbb14a4af3fdfdf3a92486c8
tianreformis <tianreformis@gmail.com>
2024-11-24 20:25:29 +0800

    pages: adding single pages teacher and student

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import Link from "next/link"
 
 
-const SingleTeacherPage = () => {
+const SingleStudentPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-4 xl:flex-row">
       {/* Left */}
@@ -24,7 +24,7 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
-              <h1 className="text-xl font-semibold">Leonard Snyder</h1>
+              <h1 className="text-xl font-semibold">Cameroon Morand</h1>
               <p className="text-sm text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
@@ -73,8 +73,8 @@ const SingleTeacherPage = () => {
               className="w-6 h-6"
               />
               <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm text-gray-400">Branch</span>
+                <h1 className="text-xl font-semibold">6th</h1>
+                <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
             {/* Card */}
@@ -87,8 +87,8 @@ const SingleTeacherPage = () => {
               className="w-6 h-6"
               />
               <div className="">
-                <h1 className="text-xl font-semibold">6</h1>
-                <span className="text-sm text-gray-400">Lesson</span>
+                <h1 className="text-xl font-semibold">18</h1>
+                <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
             {/* Card */}
@@ -101,15 +101,15 @@ const SingleTeacherPage = () => {
               className="w-6 h-6"
               />
               <div className="">
-                <h1 className="text-xl font-semibold">8</h1>
-                <span className="text-sm text-gray-400">Classe</span>
+                <h1 className="text-xl font-semibold">6A</h1>
+                <span className="text-sm text-gray-400">Class</span>
               </div>
             </div>
           </div>
         </div>
         {/* Bottom */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-          <h1> Teacher Schedule</h1>
+          <h1> Student Schedule</h1>
           <BigCalendar />
           </div>
       </div>
@@ -118,14 +118,11 @@ const SingleTeacherPage = () => {
       <div className="bg-white p-4 rounded-md ">
         <h1 className="text-xl font-semibold"></h1>
         <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-          <Link href="/" className="p-3 rounded-md bg-lamaSkyLight">Teacher Classes</Link>
-          
-          <Link href="/" className="p-3 rounded-md bg-lamaPurpleLight">Teacher Student</Link>
-          <Link href="/" className="p-3 rounded-md bg-lamaYellowLight">Teacher Lesson</Link>
-          <Link href="/" className="p-3 rounded-md bg-lama-pink-50">Teacher Exam</Link>
-          <Link href="/" className="p-3 rounded-md bg-lamaSkyLight">Teacher Assignment</Link>
-
-
+          <Link href="/" className="p-3 rounded-md bg-lamaSkyLight">Student Lessons</Link>
+          <Link href="/" className="p-3 rounded-md bg-lamaPurpleLight">Student Teachers</Link>
+          <Link href="/" className="p-3 rounded-md bg-pink-50">Student Exam</Link>
+          <Link href="/" className="p-3 rounded-md bg-lamaSkyLight">Student Assignment</Link>
+          <Link href="/" className="p-3 rounded-md bg-lamaYellowLight">Student Results</Link>
         </div>
       </div>
       <Performance/>
@@ -136,4 +133,4 @@ const SingleTeacherPage = () => {
   )
 }
 
-export default SingleTeacherPage
+export default SingleStudentPage
