f80ff7ac624dd426bee385b81961cbe0309f5b98
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-11-28 08:38:33 +0800

    adding hook form resolver min 3.54

--- src/components/Forms/TeacherForms.tsx
+++ src/components/Forms/TeacherForms.tsx
@@ -1,13 +1,43 @@
 "use client"
 
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username atleast 3 characters" })
+    .max(20, { message: "Username max 20 characters" }),
+  email: z.string().email({ message: "Invalid Email Address" }),
+  password: z.string().min(8, { message: "Password must be 8 characters" }),
+  firstName: z.string().min(3, { message: "First Name must be 3 characters" }),
+  lastName: z.string().min(3, { message: "Last Name must be 3 characters" }),
+  phone: z.string().min(10, { message: "Phone is Required" }),
+  address: z.string().min(1, { message: "Address is Required" }),
+  birthday: z.date({ message: "Birthday is Required" }),
+  sex: z.enum(["Male", "Female"], { message: "Sex is Required" }),
+  img: z.instanceof(File, { message: "Image is Required" }),
+
+
+})
+
 const TeacherForms = ({
   type,
   data
-}:{
-  type:"create"|"update"
-  data?:any
+}: {
+  type: "create" | "update"
+  data?: any
 }) => {
-  return 
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  })
+  return <form></form>
 }
 
 export default TeacherForms
