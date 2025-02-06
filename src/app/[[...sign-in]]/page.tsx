0eeaf53278d38c62211bd77da5c8f2f471f92229
tianreformis <tianreformis@gmail.com>
2025-02-06 17:32:51 +0800

    Resolve Button Login

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -3,17 +3,23 @@
 import * as Clerk from '@clerk/elements/common'
 import * as SignIn from '@clerk/elements/sign-in'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Page() {
   return <div className='flex items-center justify-center h-screen bg-lamaSkyLight'>
     <SignIn.Root>
-      <SignIn.Step name='start' className='bg-white rounded-md shadow-2xl flex flex-col gap-2'>
-        <h1 className='text-xl font-bold flex items-center gap-2'>
+      <SignIn.Step name='start' className='bg-white rounded-md shadow-2xl p-12 flex flex-col gap-2'>
+        <h1 className='text-xl font-bold flex items-center gap-2 justify-center'>
           <Image src="/logo.png" alt="" width={30} height={30} />
           SD Kudangan 2
         </h1>
-        <h2 className='text-xs text-gray-400'>
-          Masuk ke akun
+        <h2 className='text-xs text-gray-400 text-wrap'>
+          Masuk ke akun anda jikalau ada permasalahan silahkan
+          <Link href="https://wa.me/6285247037566"
+            className='font-bold text-blue-500 hover:text-blue-800 ml-2'
+          >
+            hubungi Admin
+          </Link>
         </h2>
         <Clerk.GlobalError />
         <Clerk.Field name="identifier" className='flex flex-col gap-2'>
@@ -29,7 +35,7 @@ export default function Page() {
         <Clerk.Field name="pasword" className='flex flex-col gap-2'>
           <Clerk.Label>Password</Clerk.Label>
           <Clerk.Input
-            type="text"
+            type="password"
             required
             className='p-2 rounded-md ring-1 ring-gray-300'
           />
@@ -37,7 +43,7 @@ export default function Page() {
         </Clerk.Field>
         <SignIn.Action
           submit
-          className='bg-blue-500 text-white my-1 rounded-md text-sm'
+          className='bg-blue-500 text-white my-1 rounded-md text-lg p-2 font-bold'
         >SignIn </SignIn.Action>
       </SignIn.Step>
     </SignIn.Root>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,6 +31,7 @@ const Navbar = () => {
             }
           }
         }} />
+        <UserButton />
 
       </div>
     </div>
