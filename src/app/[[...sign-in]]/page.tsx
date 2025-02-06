21f5ab57cfc577378ff7addec18d768fb3a3e77e
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2025-02-06 15:59:51 +0800

    Adding Custom Login Page with Clerk Elements at 2:26:03

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -1,10 +1,46 @@
-import { SignIn } from '@clerk/nextjs'
+'use client'
+
+import * as Clerk from '@clerk/elements/common'
+import * as SignIn from '@clerk/elements/sign-in'
+import Image from 'next/image'
 
 export default function Page() {
-  return <div className='flex items-center justify-center h-screen bg-lamaSky'>
-    <SignIn
-      forceRedirectUrl={'/admin'}
-    />
+  return <div className='flex items-center justify-center h-screen bg-lamaSkyLight'>
+    <SignIn.Root>
+      <SignIn.Step name='start' className='bg-white rounded-md shadow-2xl flex flex-col gap-2'>
+        <h1 className='text-xl font-bold flex items-center gap-2'>
+          <Image src="/logo.png" alt="" width={30} height={30} />
+          SD Kudangan 2
+        </h1>
+        <h2 className='text-xs text-gray-400'>
+          Masuk ke akun
+        </h2>
+        <Clerk.GlobalError />
+        <Clerk.Field name="identifier" className='flex flex-col gap-2'>
+          <Clerk.Label>Username</Clerk.Label>
+          <Clerk.Input
+            type="text"
+            required
+            className='p-2 rounded-md ring-1 ring-gray-300'
+          />
+          <Clerk.FieldError className='text-xs text-red-400' />
+        </Clerk.Field>
+
+        <Clerk.Field name="pasword" className='flex flex-col gap-2'>
+          <Clerk.Label>Password</Clerk.Label>
+          <Clerk.Input
+            type="text"
+            required
+            className='p-2 rounded-md ring-1 ring-gray-300'
+          />
+          <Clerk.FieldError className='text-xs text-red-400' />
+        </Clerk.Field>
+        <SignIn.Action
+          submit
+          className='bg-blue-500 text-white my-1 rounded-md text-sm'
+        >SignIn </SignIn.Action>
+      </SignIn.Step>
+    </SignIn.Root>
   </div>
 
 
