d2e8c0f8f2dbbb664fac676b095e99e2321a6b7d
tianreformis <tianreformis@gmail.com>
2025-02-07 13:18:59 +0800

    LoginPage : Addng Logo From Ajustment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
 
-const { sessionClaims, userId } = await auth();
+const { sessionClaims, userId }  = await auth();
 export const role = (sessionClaims?.metadata as { role?: string })?.role;
 export const currentUserId = userId;
