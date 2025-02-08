f60d913c01c80127f700ac18d1f605484bf1f6bf
tianreformis <tianreformis@gmail.com>
2025-02-08 20:30:20 +0800

    EventsListPage : Adding Role Condition at 2:57:54

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -2,9 +2,9 @@ import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
-import { assignmentsData, eventsData, examsData, lessonsData, resultsData, role } from "@/lib/data"
 import prisma from "@/lib/prisma"
 import { ITEM_PER_PAGE } from "@/lib/setttings"
+import { currentUserId, role } from "@/lib/utils"
 import { Class, Event, Prisma } from "@prisma/client"
 import { headers } from "next/headers"
 import Image from "next/image"
@@ -36,31 +36,33 @@ const columns = [
     accessor: "endTime",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Aksi",
-    accessor: "action",
-  },
+  ...(role === "admin"
+    ? [{
+      header: "Aksi",
+      accessor: "action",
+    }] : [])
+
 ];
 
 const renderRow = (item: EventList) => (
   <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
     <td className="flex items-center gap-4 p-4">      {item.title}</td>
-    <td className="">{item.class.name}</td>
+    <td className="">{item.class?.name || "-"}</td>
     <td className="hidden md:table-cell hover:underline">
       {new Intl.DateTimeFormat("en-US").format(item.startTime)}
     </td>
     <td className="hidden md:table-cell hover:underline">
-      {item.startTime.toLocaleTimeString("en-US",{
-        hour:"2-digit",
-        minute : "2-digit",
-        hour12 : false,
+      {item.startTime.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
       })}
     </td>
     <td className="hidden md:table-cell hover:underline">
-    {item.endTime.toLocaleTimeString("en-US",{
-        hour:"2-digit",
-        minute : "2-digit",
-        hour12 : false,
+      {item.endTime.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
       })}
     </td>
     <td>
@@ -91,7 +93,7 @@ const EventListsPage = async ({
   if (qeuryParams) {
     for (const [key, value] of Object.entries(qeuryParams)) {
       if (value !== undefined) {
-        switch (key) {          
+        switch (key) {
           case "search":
             query.title = { contains: value, mode: "insensitive" };
             break;
@@ -101,6 +103,20 @@ const EventListsPage = async ({
       }
     }
   }
+
+  //Role Conditions
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
+
+  query.OR = [ 
+    { classId: null },
+    {
+      class: roleConditions[role as keyof typeof roleConditions] || {},
+    }
+  ]
 
   const [data, count] = await prisma.$transaction([
     prisma.event.findMany({
